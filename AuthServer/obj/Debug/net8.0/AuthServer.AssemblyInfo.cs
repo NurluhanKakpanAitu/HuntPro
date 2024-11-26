4c69546edc80f021c81fc8f3e728e320d2f827b5
nurlykhankakpan <nurlykhan.kakpan@hardcode.kz>
2024-11-26 22:58:46 +0500

    update

--- AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bb55addfebcee0bdb0e37ea547aa6b7f4b7470")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
